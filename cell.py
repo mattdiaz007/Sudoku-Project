6604a000a933d10a06d9ef1933c20b67deba0960
jon vargas <vargasjonathan646@gmail.com>
2023-04-23 23:30:37 -0400

    this is me adding all the class files that I will be adding to throughout the project

---
+++ cell.py
@@ -0,0 +1,10 @@
+# this class represents a single cell in the sudoku board
+
+class Cell:
+    def __init__(self, value, row, col, screen):
+
+    def set_cell_value(self, value):
+
+    def set_sketched_value(self, value):
+
+    def draw(self):
---
+++ sudoku.py
@@ -0,0 +1,8 @@
+# this is the main executable for the suduku project
+# this will incorporate all the classes that we have created up to this point
+
+def main():
+
+
+if __name__ == '__main__':
+    main()
