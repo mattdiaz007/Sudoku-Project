9dcb4b5b8866c17378fecd55ec2f5328afb2d67c
jon vargas <vargasjonathan646@gmail.com>
2023-04-25 06:44:27 -0400

    this is the constants file that i used in my files, i need to take the time to edit the variables so that it looks cleaner

---
+++ constants.py
@@ -0,0 +1,35 @@
+# this will be a file that contains all the constants that our sudoku file can actually use to
+# define the the dimensions of our boxes, lines, and to also give us the colors that we want to use throughtout our
+# sudoku game
+
+WIDTH = 900
+HEIGHT = 900
+CELL_D = 100
+BOX_D = 300
+BOTTOM_B  = HEIGHT - CELL_D
+
+LINE_WIDTH_BOLD = 7
+LINE_WIDTH_CELL = 3
+
+
+
+WIN_LINE_WIDTH = 15
+BOARD_ROWS = 3
+BOARD_SIZE = 9
+BOARD_COLS = 3
+SQUARE_SIZE = 300
+SPACE = 55
+RED = (255, 0, 0)
+BG_COLOR = (238, 130,238,255)
+LINE_COLOR = (0,0,0,255)
+CELL_NOT_SELECTED = (0,0,0,255) #black
+CELL_SELECTED = (255, 0, 0, 255) #red
+NUM_COLOR = (255, 215, 0, 255)
+NUM_SIZE = 80
+
+COLOR_SKETCH = (128, 128, 128)
+GAME_WIN_FONT = 40
+GAME_LOSS_FONT = 40
+
+
+
