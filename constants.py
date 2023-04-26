30b2a87e77b2bbed55d033a7ab970a991df7a618
jon vargas <vargasjonathan646@gmail.com>
2023-04-26 00:23:20 -0400

    reformatted the constants file, the previous file was too disorderly

--- constants.py
+++ constants.py
@@ -1,33 +1,60 @@
 # this will be a file that contains all the constants that our sudoku file can actually use to
-# define the the dimensions of our boxes, lines, and to also give us the colors that we want to use throughtout our
+# define the the dimensions of our boxes, lines, and to also give us the colors that we want to use throughout our
 # sudoku game
+
+# centralized color variables with tuple codes that any other variable can draw upon
+
+RED = (255, 0, 0, 255)
+BLACK = (0, 0, 0, 255)
+GRAY =  (190, 190, 190, 255)
+GOLD = (255, 215, 0, 255),
+WHITE = (255, 255, 255, 255)
+VIOLET = (238, 130, 238, 255)
+
+
+
+# dimensions of the board and cells
 
 WIDTH = 900
 HEIGHT = 900
 CELL_D = 100
 BOX_D = 300
-BOTTOM_B  = HEIGHT - CELL_D
+BOTTOM_B  = HEIGHT - CELL_D # bottom box
+SQUARE_SIZE = 300
+# need to look through my class files,
+# i dont remember using SQUARE_SIZE, perhaps I used it while following Professor Zhou tic tac toe example
 
-LINE_WIDTH_BOLD = 7
-LINE_WIDTH_CELL = 3
 
 
+# additional variables binding num boxes, cols, rows
 
-WIN_LINE_WIDTH = 15
 BOARD_ROWS = 3
 BOARD_SIZE = 9
 BOARD_COLS = 3
-SQUARE_SIZE = 300
 SPACE = 55
-RED = (255, 0, 0)
-BG_COLOR = (238, 130,238,255)
-LINE_COLOR = (0,0,0,255)
-CELL_NOT_SELECTED = (0,0,0,255) #black
-CELL_SELECTED = (255, 0, 0, 255) #red
-NUM_COLOR = (255, 215, 0, 255)
-NUM_SIZE = 80
 
-COLOR_SKETCH = (128, 128, 128)
+
+
+# width of the cell (reg_width) and box lines (bold_lines)
+
+LINE_WIDTH_BOLD = 7
+LINE_WIDTH_CELL = 3
+WIN_LINE_WIDTH = 15
+
+
+
+# color of different objects in the game
+
+BG_COLOR = VIOLET
+LINE_COLOR = BLACK
+CELL_NOT_SELECTED = BLACK
+CELL_SELECTED = RED
+NUM_COLOR = GOLD
+COLOR_SKETCH = GRAY
+
+# size of font for different text surfaces
+
+NUM_SIZE = 80
 GAME_WIN_FONT = 40
 GAME_LOSS_FONT = 40
 
